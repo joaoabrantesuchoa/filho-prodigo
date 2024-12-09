be5dccdbe57bcfdfb0dec58b2546140383205b47
joao-abrantes-uchoa <joao.abrantesuchoa@gmail.com>
2024-12-09 20:10:00 -0300

    feat: new site structure

---
+++ app/page.tsx
@@ -0,0 +1,57 @@
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn, sortPosts } from "@/lib/utils";
+import { posts } from "#site/content";
+import Link from "next/link";
+import { PostItem } from "@/components/post-item";
+
+export default function Home() {
+  const latestPosts = sortPosts(posts).slice(0, 5);
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            O Filho Pródigo
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            O Filho Pródigo foi criado para as pessoas que se distanciaram da fé durante a vida e que após muito tempo
+            retornaram à ela e assim, desejam aprender os ensinamentos da Santa Igreja Católica.
+          </p>
+          <div className="flex flex-col gap-4 justify-center sm:flex-row">
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              Ver postagens
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col">
+          {latestPosts.map(
+            (post) =>
+              post.published && (
+                <li
+                  key={post.slug}
+                  className="first:border-t first:border-border"
+                >
+                  <PostItem
+                    slug={post.slug}
+                    title={post.title}
+                    description={post.description}
+                    date={post.date}
+                    tags={post.tags}
+                  />
+                </li>
+              )
+          )}
+        </ul>
+      </section>
+    </>
+  );
+}
